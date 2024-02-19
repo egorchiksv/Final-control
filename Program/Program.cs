6226b6b26fd1c508010555b55497ee81f5f12c84
Egor Saratov <npp_board@mail.ru>
2024-02-19 22:18:31 +0300

    add Program

---
+++ Program/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = new string[] {"Hello", "2", "world", ":-)"};
+
+int size (string[] arr)
+{
+    int s = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length<=3)
+        s += 1;
+    }
+    return s;
+}
+void NewArray(string[] arr)
+{
+    string[] NewArr = new string[size(arr)];
+    int j = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+    if(arr[i].Length<=3)
+    {
+        NewArr[j] = arr[i];
+        Console.Write($"{NewArr[j]} ");
+    }
+    }
+}
+NewArray(array);
